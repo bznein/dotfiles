e2e46c6b5f17e13707a4648f96295c6b76963383
Nikolas De Giorgis <nikolas.de-giorgis@10gen.com>
2020-11-18 18:42:14 +0000

    Added watch alias

--- zshrc/aliases.sh
+++ zshrc/aliases.sh
@@ -2,3 +2,4 @@ eval $(thefuck --alias fuck)
 alias branches=" git for-each-ref --sort=-committerdate refs/heads/ | awk -F'/' '{ print $NF }'"
 alias gpp="git push || fuck"
 alias feedback="git commit -m\"PR Feedback\""
+alias watch='watch '
