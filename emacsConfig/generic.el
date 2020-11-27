a2d0f8ea9d4aa625069b97acb7ef72859c6d30d4
Nikolas De Giorgis <nikolas.de-giorgis@10gen.com>
2020-11-27 10:01:28 +0000

    Emacs kill buffer and alias master

--- emacsConfig/generic.el
+++ emacsConfig/generic.el
@@ -1,3 +1,23 @@
 (delete-selection-mode 1)
 (browse-kill-ring-default-keybindings)
 (ido-mode -1)
+
+
+(defun custom-kill-buffer-fn (&optional arg)
+  "When called with a prefix argument -- i.e., C-u -- kill all interesting
+buffers -- i.e., all buffers without a leading space in the buffer-name.
+When called without a prefix argument, kill just the current buffer
+-- i.e., interesting or uninteresting."
+  (interactive "P")
+  (cond
+   ((and (consp arg) (equal arg '(4)))
+    (mapc
+     (lambda (x)
+       (let ((name (buffer-name x)))
+         (unless (eq ?\s (aref name 0))
+           (kill-buffer x))))
+     (buffer-list)))
+   (t
+    (kill-buffer (current-buffer)))))
+
+(global-set-key  (kbd "C-x x") 'custom-kill-buffer-fn)
--- zshrc/aliases.sh
+++ zshrc/aliases.sh
@@ -3,3 +3,4 @@ alias branches=" git for-each-ref --sort=-committerdate refs/heads/ | awk -F'/' 
 alias gpp="git push || fuck"
 alias feedback="git commit -m\"PR Feedback\""
 alias watch='watch '
+alias master='git switch master'
