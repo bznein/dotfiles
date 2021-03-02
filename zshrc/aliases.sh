8d77e79cdb7d2e4d90f6ddd89ee63b3e2397ac92
Nikolas De Giorgis <nikolas.de-giorgis@10gen.com>
2021-03-02 16:42:34 +0000

    Clen Container

--- zshrc/aliases.sh
+++ zshrc/aliases.sh
@@ -1,9 +1,16 @@
 eval $(thefuck --alias fuck)
+
+# GIT
 alias branches=" git for-each-ref --sort=-committerdate refs/heads/ | awk -F'/' '{ print $NF }'"
-alias gpp="git push || fuck"
 alias feedback="git commit -m\"PR Feedback\""
-alias watch='watch '
 alias master='git switch master 2> /dev/null || git switch main'
 alias wip='git commit -m"wip"'
 alias mergemaster='git commit -m"merge from master"'
 alias pullmaster='git pull origin master || git pull origin main'
+
+
+# WORK
+alias cleancontainer="docker container ls --all | grep mongodb-enterprise-operator | cut -d$' ' -f 1 | xargs -n1 docker container rm"
+
+# GENERAL
+alias watch='watch '
